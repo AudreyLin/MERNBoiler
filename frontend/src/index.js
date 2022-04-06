4a7a76d2db668b916766d9c459a5d6338169ef96
AudreyLin <audreyd.dev@gmail.com>
2022-04-06 11:43:55 -0700

    Bump react@18 and react-dom@18 Reformat frontend/src/index.js to comply with updates.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
+
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<BrowserRouter><App/></BrowserRouter>);
+//React-DOM17  - This was updated to 18 
+/*
 ReactDOM.render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
   document.getElementById('root')
 );
+*/
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
