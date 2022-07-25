53fbdb8b78d325b38822edf1e927152d57bac537
AudreyLin <audreyd.dev@gmail.com>
2022-07-25 12:32:42 -0700

    add formElements components and Contact page

--- frontend/src/pages/Contact.js
+++ frontend/src/pages/Contact.js
@@ -1,9 +1,20 @@
-import React from 'react'
+import React from 'react';
+import Input from '../shared/formElements/Input';
+import '../shared/formElements/FormStyle.css';
+import Button from '../shared/formElements/Button';
 
-const Contact = () => {
+const Contact = (props) => {
   return (
-    <div>Contact</div>
-  )
-}
+    <form className='formStyle' method='POST' action='send'>
+      <h2>Contact Me</h2>
+      <p>Tell me about your project</p>
+      <Input element='input' type='text' placeholder='First Name' />
+      <Input element='input' type='text' placeholder='Last Name' />
+      <Input element='input' type='email' placeholder='Email' />
+      <Input element='textarea' />
+      <Button>Submit</Button>
+    </form>
+  );
+};
 
 export default Contact;
